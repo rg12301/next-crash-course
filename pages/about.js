e94e0fd0edf16b909f40df9e71c5c3c36bf7a5ec
RaghavGoyal12301 <12301raghavgoyal@gmail.com>
2021-02-13 11:17:36 +0530

    v3 Include About

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,31 @@
 import Head from "next/head";
+import aboutStyles from "../styles/about.module.css";
 
 const about = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>About</title>
       </Head>
       <h1>About</h1>
-    </div>
+      <h3>
+        A <strong>Next.js</strong> crash course from&nbsp;
+        <a
+          className={aboutStyles.traversy_media}
+          href="https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA"
+        >
+          Traversy Media
+        </a>
+        .
+      </h3>
+      <p className={aboutStyles.about}>
+        This course covers all the fundamentals of&nbsp;
+        <strong>Next.js</strong>. I loved it and looking
+        forward for a course including a full stack advanced
+        WebApp project using new Next.js 10 features.&nbsp;
+        <strong>Great Thanks!!</strong>
+      </p>
+    </>
   );
 };
 
